13dc1eff1787e74be137e90f39f81ce90582af7c
Aleph Melo <alephmelo@icloud.com>
2015-07-30 16:40:08 -0300

    Add .gitignore file.

--- python_beach_boleto/core/forms.py
+++ python_beach_boleto/core/forms.py
@@ -9,26 +9,18 @@ class EnrollmentForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={
                 'placeholder': 'Nome',
-                'class': 'pure-input-1-2'
             }),
             'email': forms.TextInput(attrs={
                 'placeholder': 'Email',
-                'class': 'pure-input-1-2',
                 'type': 'email'
-            }),
-            'state': forms.Select(attrs={
-                'class': 'pure-input-1-2'
             }),
             'city': forms.TextInput(attrs={
                 'placeholder': 'Cidade',
-                'class': 'pure-input-1-2',
             }),
             'institution': forms.TextInput(attrs={
                 'placeholder': 'Instituição',
-                'class': 'pure-input-1-2'
             }),
             'degree': forms.TextInput(attrs={
                 'placeholder': 'Formação',
-                'class': 'pure-input-1-2'
             }),
         }
