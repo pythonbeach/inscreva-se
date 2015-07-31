b378f3bfcc84428506ba852ffda42b73e9939f7f
Aleph Melo <alephmelo@icloud.com>
2015-07-31 15:56:37 -0300

    Import some libs on views.py

--- python_beach_boleto/core/views.py
+++ python_beach_boleto/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from python_beach_boleto.core.forms import EnrollmentForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 
 def index(request):
@@ -7,6 +9,7 @@ def index(request):
         form = EnrollmentForm(request.POST)
         if form.is_valid():
             form.save()
+            return HttpResponseRedirect(reverse('core:index'))
     else:
         form = EnrollmentForm()
     return render(request, 'index.html', {'form': form})
