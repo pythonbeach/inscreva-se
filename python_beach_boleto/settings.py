10b34ccdb00d537ecea5bc1295ea9ee90ce5311e
Patrick Mazulo <pmazulo@gmail.com>
2015-08-04 15:28:34 -0300

    Deploy heroku

--- python_beach_boleto/settings.py
+++ python_beach_boleto/settings.py
@@ -97,10 +97,28 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+try:
+    from python_beach_boleto.local_settings import *
+except ImportError:
+    pass
--- python_beach_boleto/wsgi.py
+++ python_beach_boleto/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "python_beach_boleto.settings")
 
-application = get_wsgi_application()
+
+from dj_static import Cling
+
+
+application = Cling(get_wsgi_application())
